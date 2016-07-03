91b09fb1b8a5a4554519d79140559bee6bcc3118
jeins <mail@mjuanakbar.info>
2016-07-03 16:39:08 +0200

    add MainConfig and libraries (jwt, morgan, moment)

---
+++ app/constants/MainConfig.js
@@ -0,0 +1,15 @@
+module.exports = {
+    server: {
+        DEV: {
+            db: 'development',
+            host: 'localhost',
+            port: '8888'
+        }
+    },
+    sha256: {
+        secret: 'P3T4kAM1!!!...'  
+    },
+    token: {
+        secret: 'test123needbechange'
+    }
+};
