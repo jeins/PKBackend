43c7fdf79f68129848ed5cbbfbd02e56e47f3319
Muhammad Juan Akbar <akbar@kieback-peter.de>
2016-07-04 17:35:47 +0200

    finishing register layer

--- app/controllers/GeoServerLayerController.js
+++ app/controllers/GeoServerLayerController.js
@@ -1,14 +1,35 @@
 import PostgisProcessor from '../processors/PostgisProcessor';
+import GeoServerProcessor from '../processors/GeoServerProcessor';
+import async from 'async';
 
 module.exports = class GeoServerLayerController{
 	constructor(){
 		this.postgisProcessor = new PostgisProcessor();
+		this.geoServerProcessor = new GeoServerProcessor();
 	}
 
 	postLayer(reqBody, callback){
+		var self = this;
 		reqBody.name = reqBody.name.replace(/\s+/g, '_');
 
-		this.postgisProcessor.addLayerToPostgis(reqBody.name, reqBody.coordinates, function(result){
+		async.waterfall([
+			function(callback){
+				self.postgisProcessor.addLayerToPostgis(reqBody.name, reqBody.coordinates, function(layerCollection){
+					callback(null, layerCollection);
+				})
+			}, 
+			function(layerCollection, callback){
+				self.geoServerProcessor.registerLayer(layerCollection, reqBody.workspace, reqBody.name, function(result){
+					callback(null, "OK");
+				})
+			}
+			/*,
+			function(callback){
+				self.geoServerProcessor.getDrawTypeOfLayer()
+			} */
+		], function(error, result){
+			if(error) callback(error);
+
 			callback(result);
 		});
 	}
--- app/processors/GeoServerProcessor.js
+++ app/processors/GeoServerProcessor.js
@@ -2,42 +2,99 @@ import geoConf from '../constants/GeoserverConfig';
 import xmlBuilder from '../common/XmlBuilder';
 import http from 'http';
 import util from 'util';
+import _ from 'lodash';
+import async from 'async';
 
 module.exports = class PostgisProcessor{
 	
 	constructor(){
-		var auth = 'Basic ' + new Buffer(geoConf.rest.username + ':' + geoConf.rest.password).toString('base64');
-		this.postRequest = {
-			host: geoConf.rest.host,
-			port: geoConf.rest.port,
-			path: '',
-			method: 'POST',
-			headers: {
-				'Content-Type': 'text/xml',
-				'Content-Length': 0,
-				'Authorization': auth
-			}
-		};
 	}
 
-	_createDataStore(workspaceName, dataStoreName){
+	registerLayer(layerCollection, workspaceName, layerGroupName, callback){
+		var self = this;
+		var dataStoreName = layerGroupName;
+
+		async.waterfall([
+			function(callback){
+				self.createDataStore(workspaceName, dataStoreName, callback);
+			},
+			function(callback){
+				async.forEachOf(layerCollection, function(layerName, i, cb){
+					self.createFeatureType(workspaceName, dataStoreName, layerName, cb);
+				}, callback);
+			},
+			function(callback){
+				self.createLayerGroup(workspaceName, layerGroupName, layerCollection, callback);
+			}
+		], function(error, result){
+			if(error) callback(error);
+
+			callback("OK");
+		});
+	}
+
+	registerLayerFromShp(workspaceName, dataStoreName, shpFileName, callback){
+		var uri = '/workspaces/%s/datastores/%s/file.shp';
+
+		this._sendXmlRequest(util.format(uri, workspaceName, dataStoreName), shpFileName, callback);
+	}
+
+	getDrawTypeOfLayer(workspaceName, dataStoreName, layerName, callback){
+		var uri = '/workspaces/%s/datastores/%s/featuretypes/%s.json';
+
+	}
+
+	getLayerCollection(workspacesName, dataStoreName){
+		var uri = '/workspaces/%s/datastores/%s/featuretypes.json';
+	}
+
+	createDataStore(workspaceName, dataStoreName, callback){
 		var uri = '/workspaces/%s/datastores.xml';
 		var body = xmlBuilder.dataStore(workspaceName, dataStoreName);
 
-		this.postRequest.path = util.format(uri, workspaceName);
-		this.postRequest.headers['Content-Length'] = Buffer.byteLength(body);
+		this._sendXmlRequest(util.format(uri, workspaceName), body, callback);
+	}
+
+	createFeatureType(workspaceName, dataStoreName, featureTypeName, callback){
+		var uri = '/workspaces/%s/datastores/%s/featuretypes';
+		var body = xmlBuilder.featureType(featureTypeName);
+
+		this._sendXmlRequest(util.format(uri, workspaceName, dataStoreName), body, callback);
+	}
+
+	createLayerGroup(workspaceName, layerGroupName, layerCollection, callback){
+		var uri = '/layergroups';
+		var body = xmlBuilder.layerGroup(workspaceName, layerGroupName, layerCollection);
+
+		this._sendXmlRequest(uri, body, callback);
+	}
+
+	_sendXmlRequest(uri, body, callback){
+		var auth = 'Basic ' + new Buffer(geoConf.rest.username + ':' + geoConf.rest.password).toString('base64');
+
+		var postRequest = {
+			host: geoConf.rest.host,
+			port: geoConf.rest.port,
+			path: '/geoserver/rest' + uri,
+			method: 'POST',
+			headers: {
+				'Content-Type': 'text/xml',
+				'Authorization': auth
+			}
+		};
 
 		var req = http.request(postRequest, function(res){
-			console.log( res.statusCode );
-		   	var buffer = "";
-		   	res.on( "data", function( data ) { buffer = buffer + data; } );
-		   	res.on( "end", function( data ) { console.log( buffer ); } );
-		});
-
-		req.on('error', function(e) {
-		    console.log('problem with request: ' + e.message);
-		});
-		
+			res.setEncoding('utf8');console.log(res.statusCode);
+	      	if (res.statusCode === 201){
+	          	res.on('data', function (chunk) {});
+				callback(null);
+	      	}
+	      	else{
+	          	res.on('data', function (chunk) {
+	             	callback(chunk);
+	          	});
+	      	}
+		});		
 		req.write(body);
 		req.end();
 	}
