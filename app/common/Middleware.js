568a852b0a6854461a57c43bcd78cda500ce15e4
Muhammad Juan Akbar <akbar@kieback-peter.de>
2016-07-14 17:15:41 +0200

    fix cors

--- app/common/Middleware.js
+++ app/common/Middleware.js
@@ -5,9 +5,13 @@ import _ from 'lodash';
 module.exports = {
     enableCors: (req, res, next)=>{
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Session-Token, x-client-language");
-        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-        next();
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With, Authorization");
+
+        if ( req.method === 'OPTIONS' ) {
+            console.log('OPTIONS SUCCESS');
+            res.end();
+        } else next();
     },
     
     jwt: (req, res, next)=>{
